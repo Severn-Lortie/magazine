6a188cd1d3e7112fabb38dcb1929f142ce677eb7
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-23 16:51:50 -0500

    added router

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        light: true,
+        themes: {
+            light: {
+                primary: '#41b3a3',
+                secondary: '#c38d9e',
+                accent: 'e27d60',
+                error: '#e8a876'
+            }
+        }
+    }
 });
