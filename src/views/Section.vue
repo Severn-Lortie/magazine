4433175ff380716405d47c7913e7ed4c2a45f254
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-29 15:14:27 -0500

    gave sections banners

--- src/views/Section.vue
+++ src/views/Section.vue
@@ -1,19 +1,31 @@
 <template>
-  <app-feed :displaySection="decodeTitle(section)"/>
+  <app-feed :displaySection="decodeTitle(section)">
+      <app-banner
+      :title="decodeTitle(section)"
+      :subtitle="sectionMeta.subtitle"
+      :heroSrc="sectionMeta.heroSrc"
+      />
+  </app-feed>
 </template>
 
 <script>
 import AppFeed from '../components/AppFeed'
+import AppBanner from '../components/home/AppBanner'
+
 import TitleEncoder from '../mixins/TitleEncoder'
 
 export default {
     mixins: [TitleEncoder],
     components: {
-        AppFeed
+        AppFeed,
+        AppBanner
     },
     computed: {
         section() {
             return this.$route.params.section;
+        },
+        sectionMeta() {
+            return this.$store.state.sectionMeta[this.section];
         }
     }
 }
