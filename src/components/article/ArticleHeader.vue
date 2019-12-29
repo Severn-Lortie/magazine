2f4fb02c4437465c0152e6f9c92af388875a022c
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-29 15:13:10 -0500

    used base chip

--- src/components/article/ArticleHeader.vue
+++ src/components/article/ArticleHeader.vue
@@ -1,39 +1,40 @@
 <template>
-<v-parallax
-    height="400"
-    :src="heroSrc"
->
-    <v-row
-        align="center"
-        justify="center"
-        class="mb-12"
-    >
-        <v-col cols='5'>
-            <v-chip
-                label
-                color="secondary text-uppercase"
-            >
-                {{section}}
-            </v-chip>
-
-            <h3 class="display-3 mt-2 text-capitalize ">
-                {{title}}
-            </h3>
-
+    <v-row justify="center">
+        <v-col sm="6">
+            <base-chip 
+            :text="section" 
+            color="grey lighten-3"
+            class="mt-6" 
+            :to="`/section/${encodeTitle(section)}`"/>
+            <p class="text-capitalize headline-title mb-0">{{ title }}</p>
+            <p class="headline grey--text text--darken-2 mt-1 mb-12 text-capitalize">{{subtitle}}</p>
+            <v-img :src="heroSrc" class="mb-4">
+            </v-img>
         </v-col>
     </v-row>
-</v-parallax>
 </template>
 
 <script>
+import BaseChip from '../base/BaseChip'
+import TitleEncoder from '../../mixins/TitleEncoder'
+
 export default {
+    mixins: [TitleEncoder],
+    components: {
+        BaseChip
+    },
     props: {
         title: String,
         section: String,
-        heroSrc: String
+        heroSrc: String,
+        subtitle: String
     }
 }
 </script>
 
 <style scoped>
+.headline-title {
+    font-family: 'Droid Serif';
+    font-size: 40px;
+}
 </style>
