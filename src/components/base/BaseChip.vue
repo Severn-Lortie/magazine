e2e86d4dac8514d289fd83439900a385bdebf200
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-29 15:11:22 -0500

    refactored chips into one base chip

---
+++ src/components/base/BaseChip.vue
@@ -0,0 +1,18 @@
+<template>
+<v-chip
+    label
+    v-bind="$attrs"
+    class="text-uppercase"
+    @click.stop=""
+>
+    {{text}}
+</v-chip>
+</template>
+
+<script>
+export default {
+    props: {
+        text: String
+    }
+}
+</script>
