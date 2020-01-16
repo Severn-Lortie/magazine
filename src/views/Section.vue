a2b661e3100747297636da4eb2380795e354a4b4
Severn Lortie <severn@Severns-MacBook-Pro.local>
2020-01-16 16:49:21 -0500

    added socrates and plato ads

--- src/views/Section.vue
+++ src/views/Section.vue
@@ -1,25 +1,34 @@
 <template>
   <app-feed :displaySection="decodeTitle(section)">
+
+      <base-card class="text-capitalize mb-6" color="grey lighten-5">
+        <h3 class="display-3 font-weight-bold mb-2">{{decodeTitle(section)}}</h3>
+        <div class="headline grey--text text--darken-2">{{sectionMeta.subtitle}}</div>
+      </base-card>
+
+      <!-- 
       <app-banner
       :title="decodeTitle(section)"
       :subtitle="sectionMeta.subtitle"
       :heroSrc="sectionMeta.heroSrc"
       :main="false"
       />
+      -->
   </app-feed>
 </template>
 
 <script>
 import AppFeed from '../components/AppFeed'
-import AppBanner from '../components/home/AppBanner'
-
+//import AppBanner from '../components/home/AppBanner'
+import BaseCard from '../components/base/BaseCard'
 import TitleEncoder from '../mixins/TitleEncoder'
 
 export default {
     mixins: [TitleEncoder],
     components: {
         AppFeed,
-        AppBanner
+        BaseCard
+        //AppBanner
     },
     computed: {
         section() {
